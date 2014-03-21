89ae28c0e6f5fef041e38d69e8e5c26527bc2c49
fm-labs <flowmotionlabs@gmail.com>
2014-03-21 11:13:38 +0100

    refactor & cleanup

--- Lib/MediaThumb.php
+++ Lib/MediaThumb.php
@@ -2,6 +2,11 @@
 App::import('Vendor', 'Media.phpthumb', true, array(), 'phpThumb' . DS . 'phpthumb.class.php');
 App::uses('Inflector', 'Utility');
 
+/**
+ * Class MediaThumb
+ *
+ * Thumbnail generation for CakePHP using phpThumb
+ */
 class MediaThumb {
 
 	protected $_source;
@@ -18,7 +23,7 @@ class MediaThumb {
 
 	protected $_tmpDir;
 
-	public function __construct($source, $params = array()) {
+	public function __construct($source = null, $params = array()) {
 		// set paths
 		$this->_baseDir = (Configure::read('Media.Thumb.baseDir'))
 			? Configure::read('Media.Thumb.baseDir')
@@ -31,6 +36,10 @@ class MediaThumb {
 		$this->_tmpDir = (Configure::read('Media.Thumb.tmpDir'))
 			? Configure::read('Media.Thumb.tmpDir')
 			: TMP;
+
+		$this->_disableCache = (Configure::read('Media.Thumb.disableCache'))
+			? (bool)Configure::read('Media.Thumb.disableCache')
+			: false;
 
 		// setup default params
 		$this->_params = array(
@@ -58,13 +67,21 @@ class MediaThumb {
 			//'config_cache_maxfiles'             => null,
 			'sia'								=> "thumbnail"
 		);
-
-		$this->setSource($source);
 		$this->set($params);
 
-		//$this->_disableCache = Configure::read('Media.thumbNoCache');
+		// set source
+		if ($source) {
+			$this->setSource($source);
+		}
 	}
 
+/**
+ * Param setter
+ *
+ * @param      $key
+ * @param null $val
+ * @return $this
+ */
 	public function set($key, $val = null) {
 		if (is_array($key)) {
 			foreach ($key as $_k => $_v) {
@@ -77,53 +94,103 @@ class MediaThumb {
 		return $this;
 	}
 
-	public function setSource($filePath) {
+/**
+ * Source path setter
+ *
+ * @param $source
+ * @return $this
+ * @throws Exception
+ */
+	public function setSource($source) {
+		if (!$source) {
+			throw new Exception(__('MediaThumb: Invalid source'));
+		}
+
 		// os-specific directory separator
-		$src = preg_replace('/\//', DS, $filePath);
+		$src = preg_replace('/\//', DS, $source);
 
 		// relative paths
 		if ($src[0] != "/" && !preg_match('/^[A-Z]\:\\\/', $src)) {
 			$src = IMAGES . $src;
 		}
 
+		// verify
+		if (!is_file($src) || !is_readable($src)) {
+			throw new Exception(__('MediaThumb source file %s not accessible', $source));
+		}
+
 		$this->_source = $src;
 		return $this;
 	}
 
+/**
+ * Get path of rendered thumbnail
+ *
+ * @return mixed
+ */
+	public function getPath() {
+		return $this->_path;
+	}
+
+/**
+ * Get path of rendered thumbnail
+ *
+ * @return mixed
+ */
+	public function getUrl() {
+		return $this->_baseUrl . basename($this->_path);
+	}
+
+/**
+ * Build target filename from source and params
+ *
+ * @return string
+ */
 	protected function _getTargetFilename() {
 		$src = $this->_source;
 		$filename = Inflector::slug(basename($src));
 		$hash = md5(serialize(array($src, $this->_params)));
 		$ext = $this->_params['config_output_format'];
-		$dir = $this->_baseDir;
 
-		if (!$dir) {
-			throw new CakeException('Media thumb dir is not configured');
-		}
-
-		if (!is_dir($dir) || !is_writable($dir)) {
-			throw new CakeException(__('Media thumb dir %s does not exist or is not writable', $dir));
-		}
-
-		return $dir . $filename . "_" . $hash . '.' . $ext;
+		return $filename . "_" . $hash . '.' . $ext;
 	}
 
+/**
+ * Render thumbnail to file
+ *
+ * @throws Exception
+ */
 	public function renderToFile() {
+		// check source
 		$source = $this->_source;
-		$target = $this->_getTargetFilename();
-
-		//TODO dependency injection
-		$engine = new phpthumb();
-		$engine->setSourceFilename($source);
-		foreach ($this->_params as $key => $val) {
-			$engine->setParameter($key, $val);
+		if (!$source) {
+			throw new Exception(__('MediaThumb: No thumb source selected'));
 		}
 
+		// check thumb dir
+		$dir = $this->_baseDir;
+		if (!$dir) {
+			throw new Exception('Media thumb dir is not configured');
+		} elseif (!is_dir($dir) || !is_writable($dir)) {
+			throw new Exception(__('Media thumb dir %s does not exist or is not writable', $dir));
+		}
+
+		// check target
+		$target = $dir . $this->_getTargetFilename();
+
+		// create thumb
 		if (!file_exists($target) || $this->_disableCache === true) {
+			//TODO dependency injection
+			$engine = new phpthumb();
+			$engine->setSourceFilename($source);
+			foreach ($this->_params as $key => $val) {
+				$engine->setParameter($key, $val);
+			}
+
 			if ($engine->GenerateThumbnail()) {
 
 				if (!$engine->RenderToFile($target)) {
-					throw new CakeException('Rendering thumb to file failed');
+					throw new Exception('Rendering thumb to file failed');
 				}
 				//TODO re-evaluate; check if this is necessary
 				@chmod($target, 0644);
@@ -135,28 +202,91 @@ class MediaThumb {
 		$this->_path = $target;
 	}
 
+/**
+ * Wrapper for renderToFile()
+ *
+ * @return mixed
+ */
+	public function toFile() {
+		$this->renderToFile();
+		return $this->getPath();
+	}
+
+/**
+ * @return void
+ */
 	public function renderToBrowser() {
 		//TODO renderToBrowser() is not implemented yet
 	}
 
+/**
+ * Static wrapper to create thumbs
+ *
+ * @param       $source
+ * @param array $params
+ * @return mixed
+ */
+	public static function create($source, $params = array()) {
+		$_this = new self($source, $params);
+		$_this->renderToFile();
+		return $_this->getPath();
+	}
+
+/**
+ * Static wrapper to get thumb url
+ *
+ * @param       $source
+ * @param array $params
+ * @return mixed
+ */
+	public static function url($source, $params = array()) {
+		$_this = new self($source, $params);
+		$_this->renderToFile();
+		return $_this->getUrl();
+	}
+
+/**
+ * Legacy wrapper
+ *
+ * @return mixed
+ * @deprecated Use getPath() instead. We be removed in 2.1
+ */
 	public function getThumbPath() {
-		return $this->_path;
+		return $this->getPath();
 	}
 
+/**
+ * Legacy wrapper
+ *
+ * @return string
+ * @deprecated Use getUrl() instead. Will be removed in 2.1
+ */
 	public function getThumbUrl() {
-		return $this->_baseUrl . basename($this->_path);
+		return $this->getUrl();
 	}
 
+/**
+ * Legacy wrapper for MediaThumb::create()
+ *
+ * @param       $source
+ * @param array $params
+ * @return mixed
+ * @deprecated Use MediaThumb::create() instead. Will be removed in 2.1
+ */
 	public static function createThumb($source, $params = array()) {
-		$_this = new self($source, $params);
-		$_this->renderToFile();
-		return $_this->getThumbPath();
+		return self::create($source, $params);
 	}
 
+/**
+ * Legacy wrapper for MediaThumb::url()
+ *
+ * @param       $source
+ * @param array $params
+ * @return mixed
+ * @deprecated Use MediaThumb::url() instead. Will be removed in 2.1
+ */
 	public static function createThumbUrl($source, $params = array()) {
-		$_this = new self($source, $params);
-		$_this->renderToFile();
-		return $_this->getThumbUrl();
+		return self::url($source, $params);
 	}
 
 }
